bd36fffefda5070369cb0d01c19a316ffcfc2be8
Tony M <tony@fbpllc.com>
2021-11-07 07:46:52 -0800

    add mui

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,17 @@
 import { FunctionalComponent, h } from 'preact';
 import { Link } from 'preact-router/match';
+import { styled } from '@mui/material/styles';
 import style from './style.css';
+
+const Button = styled('button')({
+  backgroundColor: 'red'
+});
 
 const Header: FunctionalComponent = () => {
   return (
     <header class={style.header}>
       <h1>Preact App</h1>
+      <Button>button</Button>
       <nav>
         <Link activeClassName={style.active} href="/">
           Home
