e8fa6b08e8a9091a6dc15e1fc977bb8307285471
Tony M <tonym@Tonys-iMac.local>
2021-12-27 16:26:27 -0800

    karel quiz

--- src/data/quizzes/index.ts
+++ src/data/quizzes/index.ts
@@ -1,4 +1,5 @@
 import { Quiz } from '../../types';
+import KarelQuiz from './karel';
 import NumbersQuiz from './numbers';
 import PhrasesQuiz from './phrases';
 import PrepositionsQuiz from './prepositions';
@@ -8,7 +9,7 @@ import TripQuiz from './trip';
 export const quizzes: Quiz[] = prepareQuizzes();
 
 function prepareQuizzes(): Quiz[] {
-  const quizzes: Quiz[] = [...NumbersQuiz, ...PhrasesQuiz, ...PrepositionsQuiz, ...QuestionWordsQuiz, ...TripQuiz];
+  const quizzes: Quiz[] = [...KarelQuiz, ...NumbersQuiz, ...PhrasesQuiz, ...PrepositionsQuiz, ...QuestionWordsQuiz, ...TripQuiz];
 
   return quizzes.sort((a, b) => a.name.localeCompare(b.name, 'cs'));
 }
---
+++ src/data/quizzes/karel.ts
@@ -0,0 +1,46 @@
+import { Quiz, QuizQuestion } from '../../types';
+
+const KarelQuiz: Quiz[] = [
+  {
+    description: 'Opakujte v angličtině',
+    name: 'Karel IV',
+    questions: [
+      {
+        answer: ['Charles IV', 'Charles the Fourth'],
+        question:
+          'Turista, který je v Praze, často slyší: Karlův most, Karlova univerzita, Karlštejn... Ale kdo ten Karel byl? V angličtině, kdo Karel 4.?'
+      },
+      {
+        answer: ['Czech king', 'king'],
+        question: `
+          Karel 4. byl český král. Narodil se v roce 1316 a umřel v roce 1378.
+          Jeho otec byl y Lucemburska a jeho matka byla Češka.
+          Karel studoval ve Francii a do Prahy přišel v roce 1333.
+          V angličtině, co Karel 4.?
+        `
+      },
+      {
+        answer: ['1357'],
+        question: `
+          Karel 4. byl velmi energický a inteligentní. Byl štíhlý a vysoký - měřil 175 centimetrů.
+          To bylo neobvyklé, protože lidé byli malí. Byl dobrý politik a organizátor, ale byl taky religiózní a pověrčivý.
+          Když plánoval most, hledal spolu s astrology speciální čas a den. Začínal stavět most 9. 7. 1357 v 5 hodin 31 minut.
+          Proč? Protože to datum dělá "most" - 1 3 5 7 9 7 5 3 1.
+          Co roce Karel postavil most?
+        `
+      },
+      {
+        answer: ['yes'],
+        question: `
+          Karel č. měl 4 manželky.
+          Poslední z nich, krásnou Elišku Pomořanskou, která byla silná jako muž, můžete vidět vidět ve filmu Noc na Karlštejně.
+          Karel měl rád Prahu a hodně pro město a pro celou zemi dělal.
+          Byl to dobrý král.
+          Byl Karel č. dobrý král?
+        `
+      }
+    ]
+  }
+];
+
+export default KarelQuiz;
