6c58ccbb0c3a3cddd58d16f3d6bb41e25ffb46ed
Tony M <tony@fbpllc.com>
2022-02-08 13:57:36 -0800

    hair quiz

---
+++ src/data/adjectives/hair.ts
@@ -0,0 +1,84 @@
+import { Quiz } from '../../types';
+
+const HairQuiz: Quiz[] = [
+  {
+    description: 'Jak to popisujeme?',
+    name: 'Popis vlasy',
+    questions: [
+      {
+        answer: ['raven hair'],
+        question: 'havraní vlasy'
+      },
+      {
+        answer: ['black hair'],
+        question: 'černé vlasy'
+      },
+      {
+        answer: ['blonde hair'],
+        question: 'blonďaté vlasy'
+      },
+      {
+        answer: ['chestnut hair'],
+        question: 'kaštanové vlasy'
+      },
+      {
+        answer: ['brown hair'],
+        question: 'hnědé vlasy'
+      },
+      {
+        answer: ['dirty blonde hair'],
+        question: 'špinavě blond vlasy'
+      },
+      {
+        answer: ['red hair'],
+        question: 'zrzavé vlasy'
+      },
+      {
+        answer: ['ginger hair'],
+        question: 'rezavé vlasy'
+      },
+      {
+        answer: ['grey hair'],
+        question: 'šedé vlasy'
+      },
+      {
+        answer: ['bald'],
+        question: 'pleš'
+      },
+      {
+        answer: ['long hair'],
+        question: 'dlouhé vlasy'
+      },
+      {
+        answer: ['short hair'],
+        question: 'krátké vlasy'
+      },
+      {
+        answer: ['straight hair'],
+        question: 'rovné vlasy'
+      },
+      {
+        answer: ['wavy hair'],
+        question: 'vlnité vlasy'
+      },
+      {
+        answer: ['curly hair'],
+        question: 'kudrnaté vlasy'
+      },
+      {
+        answer: ['fringe', 'bangs'],
+        question: 'ofina'
+      },
+      {
+        answer: ['thick hair'],
+        question: 'husté vlasy'
+      },
+      {
+        answer: ['thin hair'],
+        question: 'řídké vlasy'
+      }
+    ]
+  }
+];
+
+export default HairQuiz;
--- src/data/adjectives/index.ts
+++ src/data/adjectives/index.ts
@@ -1,11 +1,12 @@
 import { Quiz } from '../../types';
 import ClothingQuiz from './clothing';
 import ColorsQuiz from './colors';
+import HairQuiz from './hair';
 
 export const quizzes: Quiz[] = prepareQuizzes();
 
 function prepareQuizzes(): Quiz[] {
-  const quizzes: Quiz[] = [...ClothingQuiz, ...ColorsQuiz];
+  const quizzes: Quiz[] = [...ClothingQuiz, ...ColorsQuiz, ...HairQuiz];
 
   return quizzes.sort((a, b) => a.name.localeCompare(b.name, 'cs'));
 }
