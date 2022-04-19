b068c6fa81bf837e15c6b2eb8a62e74358caeae6
Tony M <tony@fbpllc.com>
2022-04-19 09:37:46 -0700

    dative quiz

---
+++ src/data/nouns/dative.ts
@@ -0,0 +1,28 @@
+import { Quiz } from '../../types';
+
+const DativeTranslationQuiz: Quiz[] = [
+  {
+    description: 'komu, čemu?',
+    name: 'Dative',
+    questions: [
+      {
+        answer: ['because of'],
+        question: 'kvůli'
+      },
+      {
+        answer: ['opposite', 'across from'],
+        question: 'naproti'
+      },
+      {
+        answer: ['to', 'towards'],
+        question: 'k'
+      },
+      {
+        answer: ['thanks to', 'thanks to whom'],
+        question: 'díky'
+      }
+    ]
+  }
+];
+
+export default DativeTranslationQuiz;
--- src/data/nouns/index.ts
+++ src/data/nouns/index.ts
@@ -1,11 +1,12 @@
 import { Quiz } from '../../types';
 import IdentifiersTranslationQuiz from './identifiers';
 import GenitiveTranslationQuiz from './genitive';
+import DativeTranslationQuiz from './dative';
 
 export const quizzes: Quiz[] = prepareQuizzes();
 
 function prepareQuizzes(): Quiz[] {
-  const quizzes: Quiz[] = [...GenitiveTranslationQuiz, ...IdentifiersTranslationQuiz];
+  const quizzes: Quiz[] = [...DativeTranslationQuiz, ...GenitiveTranslationQuiz, ...IdentifiersTranslationQuiz];
 
   return quizzes.sort((a, b) => a.name.localeCompare(b.name, 'cs'));
 }
